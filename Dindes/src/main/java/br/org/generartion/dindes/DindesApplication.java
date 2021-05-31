a7a688e89bfe7a99067e38e4a15145668995d58a
Alex Avelino <alourenco08@gmail.com>
2021-05-31 09:57:44 -0300

    Criação da tabela Tema

---
+++ Dindes/src/main/java/br/org/generartion/dindes/DindesApplication.java
@@ -0,0 +1,13 @@
+package br.org.generartion.dindes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DindesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DindesApplication.class, args);
+	}
+
+}
---
+++ Dindes/src/main/java/br/org/generartion/dindes/model/Tema.java
@@ -0,0 +1,63 @@
+package br.org.generartion.dindes.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_tema")
+public class Tema {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull(message = "Atributo obrigatório")
+	@Size(min = 2, max = 255)
+	private String tema;
+	
+	@NotNull
+	@Size(min = 2, max = 255)
+	private String area;
+	
+	@Size(min = 5, max = 500)
+	private String descricao;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTema() {
+		return tema;
+	}
+
+	public void setTema(String tema) {
+		this.tema = tema;
+	}
+
+	public String getArea() {
+		return area;
+	}
+
+	public void setArea(String area) {
+		this.area = area;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
+}
